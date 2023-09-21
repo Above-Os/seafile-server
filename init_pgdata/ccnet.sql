495e449a8ff2d81d47b7350079f1b89e21c19e08
Wang Rongxiang <wangrongxiang@bytetrade.io>
2023-09-21 19:45:31 +0800

    pgdb build v0.0.1

--- init_pgdata/ccnet.sql
+++ init_pgdata/ccnet.sql
@@ -16,6 +16,28 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+--
+-- Name: ccnet; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE ccnet WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'C.UTF-8';
+
+
+ALTER DATABASE ccnet OWNER TO postgres;
+
+\connect ccnet
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
